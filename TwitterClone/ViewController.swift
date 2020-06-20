98faaa25983405b4917045bb2441e4177dbbd8ca
sudhansudwivedi004 <Sudhanshu@iimjobs.com>
2020-06-20 16:29:11 +0530

    Created a project

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Sudhanshu-Air on 20/06/20.
+//  Copyright © 2020 Sudhanshu-Air. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
